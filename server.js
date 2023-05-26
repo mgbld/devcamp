a555e10120b3ac78978b6000a84ed63137af82a6
mgbld <112904350+mgbld@users.noreply.github.com>
2023-05-26 17:28:15 -0600

    Add logger middware

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+// const logger = require('./middlewares/logger');
+const connectDB = require('./config/db');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -8,14 +11,30 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({
   path: './config/config.env',
 });
+
+// Connect to database
+connectDB();
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+// app.use(logger);
 
 // Nount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
